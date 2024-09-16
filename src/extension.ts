83a842b1282275786d0401be9f4b6593291d5c1a
André Ribeiro <andremezzaliraribeiro@gmail.com>
2024-09-15 23:48:49 -0300

    first commit

---
+++ src/extension.ts
@@ -0,0 +1,57 @@
+import * as vscode from 'vscode';
+
+function inferType(value: any): string {
+  if (Array.isArray(value)) {
+    return `${inferType(value[0])}[]`;
+  } else if (typeof value === 'string') {
+    return 'string';
+  } else if (typeof value === 'number') {
+    return 'number';
+  } else if (typeof value === 'boolean') {
+    return 'boolean';
+  } else if (typeof value === 'object' && value !== null) {
+    return 'object';
+  } else {
+    return 'any';
+  }
+}
+
+function generateInterface(jsonData: any, interfaceName: string = 'Root'): string {
+  let result = `interface ${interfaceName} {\n`;
+
+  for (let key in jsonData) {
+    const type = inferType(jsonData[key]);
+    result += `  ${key}: ${type};\n`;
+  }
+
+  result += `}\n`;
+  return result;
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand('extension.convertJsonToInterface', () => {
+    const editor = vscode.window.activeTextEditor;
+
+    if (editor) {
+      const document = editor.document;
+      const selection = editor.selection;
+      const jsonText = document.getText(selection) || document.getText();
+
+      try {
+        const jsonData = JSON.parse(jsonText);
+
+        const tsInterface = generateInterface(jsonData);
+
+        editor.edit((editBuilder) => {
+          editBuilder.replace(selection.isEmpty ? new vscode.Range(0, 0, document.lineCount, 0) : selection, tsInterface);
+        });
+      } catch (error) {
+        vscode.window.showErrorMessage('Invalid JSON. Please check your JSON format.');
+      }
+    }
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
